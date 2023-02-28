23b35af7d184e2e7fd961b9cd7517baf99434b4d
Meng Le <mengle.zhang@gmail.com>
2023-02-28 12:06:13 +0000

    added map instructions

--- ui.R
+++ ui.R
@@ -32,6 +32,7 @@ download_data <-
     downloadLink('download_app_data', 'Download dashboard data')
   }
 
+
 map_input_panel <-
   function(){
      selectInput(
@@ -160,15 +161,20 @@ fluidPage(
                   
                   tabPanel("mapTab", 
                            sidebarLayout(
-                             sidebarPanel(map_input_panel()),
+                             sidebarPanel(
+                               h4(strong("Explore Frontiers")),
+                               map_input_panel(),
+                               
+                               p(
+                                 'Colours represent wider areas with high numbers of frontiers. Drag the map and zoom in to see the location of frontiers'
+                               )
+                             ),
                              mainPanel(
                                leafletOutput("map", height = 1000))
                              ) 
                            ),
-                  # tabPanel("mapTab", div(class="outer", leafletOutput("map", height = 1000))),
-  #                tabPanel("plotTab", plotlyOutput("plot")),
                   summary_panel('Summary and plots'),
-  diag_panel('diagnostics')
+    diag_panel('diagnostics')
                   )                
       )
       
